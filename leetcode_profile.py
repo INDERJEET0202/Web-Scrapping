a2427125c456c585ace7f22cb27246f4ab6268ed
Indrajit Pal <palindrajit10@gmail.com>
2023-03-27 20:56:59 +0530

    leetcode profile scrapper

--- leetcode_profile.py
+++ leetcode_profile.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 url = "https://leetcode.com/indrajit10/"
 
 response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+soup = BeautifulSoup(response.text, "html.parser") 
 
 # print(soup.prettify())
 
